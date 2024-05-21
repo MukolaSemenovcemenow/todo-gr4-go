fe360380a63f0fbdcd4d7ae941c92b05c890e9ba
BohdanBoriak <lightraven72@gmail.com>
2024-05-21 18:49:04 +0300

    WIP: Added task controller

--- internal/infra/database/task_repository.go
+++ internal/infra/database/task_repository.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+	"github.com/upper/db/v4"
 )
 
 const TasksTableName = "tasks"
@@ -18,4 +19,60 @@ type task struct {
 	CreatedDate time.Time         `db:"created_date"`
 	UpdatedDate time.Time         `db:"updated_date"`
 	DeletedDate *time.Time        `db:"deleted_date"`
+}
+
+type TaskRepository interface {
+	Save(t domain.Task) (domain.Task, error)
+}
+
+type taskRepository struct {
+	coll db.Collection
+	sess db.Session
+}
+
+func NewTaskRepository(session db.Session) TaskRepository {
+	return taskRepository{
+		coll: session.Collection(TasksTableName),
+		sess: session,
+	}
+}
+
+func (r taskRepository) Save(t domain.Task) (domain.Task, error) {
+	tsk := r.mapDomainToModel(t)
+	tsk.CreatedDate = time.Now()
+	tsk.UpdatedDate = time.Now()
+	err := r.coll.InsertReturning(&tsk)
+	if err != nil {
+		return domain.Task{}, err
+	}
+	t = r.mapModelToDomain(tsk)
+	return t, nil
+}
+
+func (r taskRepository) mapDomainToModel(t domain.Task) task {
+	return task{
+		Id:          t.Id,
+		UserId:      t.UserId,
+		Title:       t.Title,
+		Description: t.Description,
+		Deadline:    t.Deadline,
+		Status:      t.Status,
+		CreatedDate: t.CreatedDate,
+		UpdatedDate: t.UpdatedDate,
+		DeletedDate: t.DeletedDate,
+	}
+}
+
+func (r taskRepository) mapModelToDomain(t task) domain.Task {
+	return domain.Task{
+		Id:          t.Id,
+		UserId:      t.UserId,
+		Title:       t.Title,
+		Description: t.Description,
+		Deadline:    t.Deadline,
+		Status:      t.Status,
+		CreatedDate: t.CreatedDate,
+		UpdatedDate: t.UpdatedDate,
+		DeletedDate: t.DeletedDate,
+	}
 }
